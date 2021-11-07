eb3785bfd4571c140b6d173d70360ca69aa5e834
Rikesh Mehta <55822176+rikeshnmehta@users.noreply.github.com>
2021-11-07 13:43:57 -0800

    added potion use

--- pokemon_gen.py
+++ pokemon_gen.py
@@ -26,8 +26,11 @@ def moveset_generator():
 class Pokemon:
     def __init__(self, name=""):
         self.name = name
-        self.hp = HEALTH_MIN + int(random.random() * (HEALTH_MAX - HEALTH_MIN))
+        self.max_hp = HEALTH_MIN + int(random.random() * (HEALTH_MAX - HEALTH_MIN))
+        self.hp = 0
         self.moveset = moveset_generator()
+        self.potion_count = 3
+        self.ready = False
 
 
 user_pokemon = Pokemon()
@@ -36,30 +39,32 @@ enemy_pokemon = Pokemon()
 
 def battle_end_dialogue(result):
     if result == "win":
+        user_pokemon.ready = False
         with open("media/text/enemy_lose_battle.txt") as r:
             enemy_lose_battle_text = r.read()
         post_battle_model = markovify.Text(enemy_lose_battle_text)
         return (
-            "Your "
+            "\nYour "
             + user_pokemon.name[:-1]
             + " used "
             + random.choice(user_pokemon.moveset)[:-1]
-            + " and won the the battle!\n"
+            + ' and won the the battle!\n"'
             + post_battle_model.make_sentence()
-            + "\n\n Use generate to play again."
+            + '"\n\n Use generate to play again.'
         )
     else:
+        user_pokemon.ready = False
         with open("media/text/enemy_win_battle.txt") as r:
             enemy_win_battle_text = r.read()
         post_battle_model = markovify.Text(enemy_win_battle_text)
         return (
-            "Enemy "
+            "\nEnemy "
             + enemy_pokemon.name[:-1]
             + " used "
             + random.choice(enemy_pokemon.moveset)[:-1]
-            + " and won the battle!\n"
+            + ' and won the battle!\n"'
             + post_battle_model.make_sentence()
-            + "\n\n Use generate to play again."
+            + '"\n\n Use generate to play again.'
         )
 
 
@@ -91,10 +96,19 @@ async def scenario_response(ctx):
         await ctx.send(file=discord.File("media/attitudes/no.gif"))
         await ctx.send("No, I don't think I will.", tts=TEXT_TO_SPEECH)
     else:
-        user_pokemon.hp = HEALTH_MIN + int(random.random() * (HEALTH_MAX - HEALTH_MIN))
+        user_pokemon.max_hp = HEALTH_MIN + int(
+            random.random() * (HEALTH_MAX - HEALTH_MIN)
+        )
+        user_pokemon.hp = user_pokemon.max_hp
         user_pokemon.moveset = moveset_generator()
+        user_pokemon.ready = True
+
+        enemy_pokemon.max_hp = HEALTH_MIN + int(
+            random.random() * (HEALTH_MAX - HEALTH_MIN)
+        )
+        enemy_pokemon.hp = enemy_pokemon.max_hp
         enemy_pokemon.moveset = moveset_generator()
-        enemy_pokemon.hp = HEALTH_MIN + int(random.random() * (HEALTH_MAX - HEALTH_MIN))
+        enemy_pokemon.ready = True
         await ctx.send(file=discord.File(random.choice(glob.glob("media/sprites/*"))))
         # Markov chain generate new original pokemon name
         user_pokemon.name = name_model.make_short_sentence(10)
@@ -113,6 +127,9 @@ async def scenario_response(ctx):
             + " and has "
             + str(user_pokemon.hp)
             + " health points."
+            + "\n\nYou have "
+            + str(user_pokemon.potion_count)
+            + " potions. Use them with the potion command."
         )
         await ctx.send(response, tts=TEXT_TO_SPEECH)
 
@@ -122,47 +139,150 @@ async def scenario_response(ctx):
     help="After generating a scenario, this will execute one round of attacks.",
 )
 async def scenario_response(ctx):
-    user_move = random.choice(user_pokemon.moveset)
-    enemy_move = random.choice(enemy_pokemon.moveset)
-    user_damage = int(random.random() * 50)
-    enemy_damage = int(random.random() * 50)
-    if random.random() <= 0.2:
-        user_damage += 50
-        await ctx.send("CRITICAL HIT!", tts=TEXT_TO_SPEECH)
+    if user_pokemon.ready:
+        user_move = random.choice(user_pokemon.moveset)
+        enemy_move = random.choice(enemy_pokemon.moveset)
+        user_damage = int(random.random() * 50)
+        enemy_damage = int(random.random() * 50)
+        if random.random() <= 0.2:
+            user_damage += 50
+            await ctx.send("CRITICAL HIT!", tts=TEXT_TO_SPEECH)
 
-    user_pokemon.hp -= enemy_damage
-    enemy_pokemon.hp -= user_damage
+        user_miss = random.random() <= 0.15
+        enemy_miss = random.random() <= 0.2
 
-    if user_pokemon.hp <= 0:
-        response = battle_end_dialogue(result="lose")
-    elif enemy_pokemon.hp <= 0:
-        response = battle_end_dialogue(result="win")
+        if user_miss:
+            response = (
+                "Your fakemon, "
+                + user_pokemon.name[:-1]
+                + ", used "
+                + user_move[:-1]
+                + " but missed.\n"
+            )
+            user_damage = 0
+        else:
+            response = (
+                "Your fakemon, "
+                + user_pokemon.name[:-1]
+                + ", used "
+                + user_move[:-1]
+                + " and did "
+                + str(user_damage)
+                + " damage.\n"
+            )
+        await ctx.send(response, tts=TEXT_TO_SPEECH)
+
+        if enemy_miss:
+            response = (
+                "Enemy fakemon, "
+                + enemy_pokemon.name[:-1]
+                + ", used "
+                + enemy_move[:-1]
+                + " but missed.\n"
+            )
+            enemy_damage = 0
+        else:
+            response = (
+                "Enemy fakemon, "
+                + enemy_pokemon.name[:-1]
+                + ", used "
+                + enemy_move[:-1]
+                + " and did "
+                + str(enemy_damage)
+                + " damage.\n"
+            )
+        await ctx.send(response, tts=TEXT_TO_SPEECH)
+
+        user_pokemon.hp -= enemy_damage
+        enemy_pokemon.hp -= user_damage
+
+        if user_pokemon.hp <= 0:
+            response = battle_end_dialogue(result="lose")
+        elif enemy_pokemon.hp <= 0:
+            response = battle_end_dialogue(result="win")
+        else:
+            response = (
+                "Your fakemon, "
+                + user_pokemon.name[:-1]
+                + ", has "
+                + str(user_pokemon.hp)
+                + " health points left.\n"
+                + "Enemy fakemon, "
+                + enemy_pokemon.name[:-1]
+                + ", has "
+                + str(enemy_pokemon.hp)
+                + " health points left.\n"
+            )
+        await ctx.send(response, tts=TEXT_TO_SPEECH)
     else:
-        response = (
-            user_pokemon.name[:-1]
-            + " used "
-            + user_move[:-1]
-            + " and did "
-            + str(user_damage)
-            + " damage.\n"
-            + enemy_pokemon.name[:-1]
-            + " used "
-            + enemy_move[:-1]
-            + " and did "
-            + str(enemy_damage)
-            + " damage.\n\n"
-            + "Your fakemon, "
-            + user_pokemon.name[:-1]
-            + ", has "
-            + str(user_pokemon.hp)
-            + " health points left.\n"
-            + "Enemy fakemon, "
-            + enemy_pokemon.name[:-1]
-            + ", has "
-            + str(enemy_pokemon.hp)
-            + " health points left.\n"
+        await ctx.send(
+            "Run generate to create a new scenario first.", tts=TEXT_TO_SPEECH
         )
-    await ctx.send(response, tts=TEXT_TO_SPEECH)
+
+
+@bot.command(
+    name="potion",
+    help="After generating a scenario, this will use one potion to heal your fakemon by 20 health points.",
+)
+async def potion_use(ctx):
+    if user_pokemon.ready:
+        new_hp = user_pokemon.hp + 20
+        if new_hp > user_pokemon.max_hp:
+            user_pokemon.hp = user_pokemon.max_hp
+        else:
+            user_pokemon.hp = new_hp
+
+        response = (
+            "You used a potion.\n"
+            + user_pokemon.name[:-1]
+            + " now has "
+            + str(user_pokemon.hp)
+            + " health points.\n"
+        )
+        await ctx.send(response, tts=TEXT_TO_SPEECH)
+
+        enemy_move = random.choice(enemy_pokemon.moveset)
+        if random.random() <= 1.2:
+            # Attack missed
+            response = (
+                "Enemy fakemon, "
+                + enemy_pokemon.name[:-1]
+                + ", used "
+                + enemy_move[:-1]
+                + " but missed.\n"
+            )
+            await ctx.send(response, tts=TEXT_TO_SPEECH)
+        else:
+            enemy_damage = int(random.random() * 50)
+            user_pokemon.hp -= enemy_damage
+
+            if user_pokemon.hp <= 0:
+                response = battle_end_dialogue(result="lose")
+            else:
+                response = (
+                    "Enemy fakemon, "
+                    + enemy_pokemon.name[:-1]
+                    + ", used "
+                    + enemy_move[:-1]
+                    + " and did "
+                    + str(enemy_damage)
+                    + " damage.\n\n"
+                    + "Your fakemon, "
+                    + user_pokemon.name[:-1]
+                    + ", has "
+                    + str(user_pokemon.hp)
+                    + " health points left.\n"
+                    + "Enemy fakemon, "
+                    + enemy_pokemon.name[:-1]
+                    + ", has "
+                    + str(enemy_pokemon.hp)
+                    + " health points left.\n"
+                )
+            await ctx.send(response, tts=TEXT_TO_SPEECH)
+    else:
+        await ctx.send(
+            "Run generate to create a new scenario first.", tts=TEXT_TO_SPEECH
+        )
 
 
 bot.run(TOKEN)
